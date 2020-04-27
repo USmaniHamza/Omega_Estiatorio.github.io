8018bce3bb62f1f356fde2a94f3c50eb2bd98d45
Abdul Momen Usmani <hamzahusmani00@gmail.com>
2020-04-28 02:01:13 +0600

    React Components part 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; // THIS JS  FILE IS THE PLAYGROUND FOR EVERYTHING
 import { Navbar,NavbarBrand } from 'reactstrap'; //   watch the whole video with patience please
 import Menu from './Components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 //ekhane shob structurewise shajaylam
 class App extends Component {
-  render(){
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes:DISHES
+    };
+  }
+  render(){ //since extension thakle render return must dite hobe nahole hobe na
     return (
       <div> {/* class name app has been removed */}
         <Navbar dark color="primary">
@@ -13,7 +20,7 @@ class App extends Component {
           </div>
         </Navbar> {/* the menu component placed here */}
 
-        <Menu /> {/* the menu component placed here */}
+        <Menu dishes={this.state.dishes} /> {/* props, and the fact that dishes is a made up variable PATHAYDILAM SHAJAY DEYAR JONNO*/}
       </div>
     );
   } 
--- src/Components/MenuComponent.js
+++ src/Components/MenuComponent.js
@@ -1,81 +1,74 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 //menu so it would return a list of dishes in my restaurant
-class Menu extends Component{
+class Menu extends Component{ //this is a component
    
     constructor(props){
         super(props); //immediate parent class constructor
 
-        this.state = { //its a property i guess
-            dishes: [  //dishes is a variable
-            {
-                id: 0,
-                name:'Uthappizza',
-                image: 'assets/images/uthappizza.png',
-                category: 'mains',
-                label:'Hot',
-                price:'4.99',
-                description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        
-            },
-             {
-                id: 1,
-                name:'Zucchipakoda',
-                image: 'assets/images/zucchipakoda.png',
-                category: 'appetizer',
-                label:'',
-                price:'1.99',
-                description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        
-            },
-             {
-                id: 2,
-                name:'Vadonut',
-                image: 'assets/images/vadonut.png',
-                category: 'appetizer',
-                label:'New',
-                price:'1.99',
-                description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        
-            },
-             {
-                id: 3,
-                name:'ElaiCheese Cake',
-                image: 'assets/images/elaicheesecake.png',
-                category: 'dessert',
-                label:'',
-                price:'2.99',
-                description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'     
-
-             }
-            ],
-        };
+        this.state = { //its a property i guess, nijer variable
+            selectedDish: null
+        }
     }
+    onDishSelect(dish){
+         this.setState({selectedDish:dish}); //AJKEE if we want to update the variable over 
+
+    }
+    renderDish(dish){ //the variable that is going to be passed here
+        // AJKE so if the selected dish is null then else e giye khali dekhabo
+        //AJKE ar jodi null na hoy tahole if e jabo as said per condition
+         if(dish!=null){
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name}  />
+                    <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+         }
+         else{
+             return(
+                <div></div>
+             );
+         }
+
+    }
+
 render(){
 
-    const menu=this.state.dishes.map((dish) => {   //so ami variable ta ke ekhane banailam jaate ami return e variable ta dite pari
+    const menu=this.props.dishes.map( (dish) => {   //so ami variable ta ke ekhane banailam jaate ami return e variable ta dite pari &dish is a variable
         return( // ekhane bolte gele amra menu ta ke render ar structure korlam jate amra pore use korte pari
             //so this was out component
             //dish ta kemne shajano thakbe oita bollam
             //array hishabe ekhane anlam arki
-            <div key={dish.id} className="col-12 mt-5">             
-              <Media tag="li">
-                  <Media left middle>
-                      <Media object src={dish.image} alt={dish.name}  />
-                  </Media>
-                        <Media body className="ml-5">
-                            <Media heading>{dish.name}</Media>
-                            <p>{dish.description}</p>
-                        </Media>
-              </Media>
+            //ETA VARIABLE CALL ER RETURN over here which is menu
+            <div key={dish.id} className="col-12 col-md-5 m-1">             
+              <Card onClick={()=> this.onDishSelect(dish)}>  {/* BUT HOW DOES IT KNOW THAT IT IS A LOOP   */}
+                  
+                        <CardImg width="100%" src={dish.image} alt={dish.name}  />
+                                      
+                        <CardImgOverlay body className="ml-4"> {/*ml to slide the title left and right */}
+                            <CardTitle>{dish.name}</CardTitle>
+                            
+                        </CardImgOverlay>
+              </Card>
             </div>
               );
     }) ; //menu defined as a JS variable
     //mapping
 
         return( // seems like we can use JS variables inside my JSX menu is a JS variable
+            //ETA RENDER ER RETURN
             <div className="container">
                 <div className="row"> 
-                    <Media list> {/* list na dile horizontal hoye jabe */}
+                     {/* list na dile horizontal hoye jabe */}
                         {menu} 
-                    </Media>
+                    
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
             ); 
