33d208593968f38a2bf64764ee0cf8c4b879cca4
Abdul Momen Usmani <hamzahusmani00@gmail.com>
2020-04-27 13:40:30 +0600

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Navbar,NavbarBrand } from 'reactstrap'; //learn to watch the whole video with patience please
 import './App.css';
+
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title" alt="logo">Welcome to React</h1>
-        </header>
-          <p className="App-intro"> To get started,
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
       </div>
     );
   } //kono extension kore nai just like in the tutorial
